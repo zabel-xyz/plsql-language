73da9f2264b22c541d03abae4b163b82e10fa099
Danieleeee <danieleee84@gmail.com>
2017-06-28 18:22:48 +0200

    Update plsqlDocumentSymbol.provider.ts

--- src/plsqlDocumentSymbol.provider.ts
+++ src/plsqlDocumentSymbol.provider.ts
@@ -5,7 +5,7 @@ export class PLSQLDocumentSymbolProvider implements vscode.DocumentSymbolProvide
     public provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.SymbolInformation[] {
         const regComment = `(?:\\/\\*[\\s\\S]*?\\*\\/)|(?:--.*)`;
         const regFind = `${regComment}|(?:create(?:\\s+or\\s+replace)?\\s+)?((\\b(?:function|procedure|package)\\b(?:\\s+body)?)\\s+(?:\\w+\\.)?\\w+)`;
-        const regConst = `${regComment}|(\\bbegin\\b)?\\s*(\\b\\w+\\b)\\s*(\\bconstant\\b)+\\s*(\\w*)`;
+        const regConst = `${regComment}|(\\b\\w+.*\\b)\\s*\\s+(\\w*)\\((\\bfunction|procedure\\b)?`;
         const regexp = new RegExp(regFind, 'gi');
         const regexpCons = new RegExp(regConst, 'gi');
 
@@ -25,16 +25,18 @@ export class PLSQLDocumentSymbolProvider implements vscode.DocumentSymbolProvide
         let constant;
         while (constant = regexpCons.exec(text)) {
             if (constant[2]) {
-                if (constant[1]){
-                    null;
-                }
-                else{
+                if (constant[1].toLowerCase() !== "function" && constant[1].toLowerCase() !== "procedure"){
                     let line = document.lineAt(document.positionAt(constant.index));
+                    let icon;
+                    if(constant[1].toLowerCase().indexOf("constant") > 0)
+                       icon = vscode.SymbolKind.Constant;
+                    else icon = vscode.SymbolKind.Key;
                     let symbolInfo = new vscode.SymbolInformation(
-                        constant[3]+" "+constant[2]+" "+constant[4], this.getSymbolKind(constant[3].toLowerCase()),
+                        constant[1]+" "+constant[2], icon,
                         new vscode.Range(line.range.start, line.range.end));
                     symbols.push(symbolInfo);
                 }
+                else return symbols;
             }
         }
         return symbols;
@@ -46,8 +48,6 @@ export class PLSQLDocumentSymbolProvider implements vscode.DocumentSymbolProvide
             return vscode.SymbolKind.Function;
         else if (type === 'procedure')
             return vscode.SymbolKind.Method;
-        else if (type === 'constant')
-            return vscode.SymbolKind.Constant;
         else
             return vscode.SymbolKind.Package; // Specification or Body
     }
