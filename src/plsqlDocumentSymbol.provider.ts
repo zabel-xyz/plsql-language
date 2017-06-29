51b41124b118068e5dce1afcd3e513c295dc4f7d
Danieleeee <danieleee84@gmail.com>
2017-06-29 10:19:05 +0200

    Update plsqlDocumentSymbol.provider.ts

--- src/plsqlDocumentSymbol.provider.ts
+++ src/plsqlDocumentSymbol.provider.ts
@@ -5,7 +5,7 @@ export class PLSQLDocumentSymbolProvider implements vscode.DocumentSymbolProvide
     public provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.SymbolInformation[] {
         const regComment = `(?:\\/\\*[\\s\\S]*?\\*\\/)|(?:--.*)`;
         const regFind = `${regComment}|(?:create(?:\\s+or\\s+replace)?\\s+)?((\\b(?:function|procedure|package)\\b(?:\\s+body)?)\\s+(?:\\w+\\.)?\\w+)`;
-        const regConst = `${regComment}|(\\b\\w+.*\\b)\\s*\\s+(\\w*)\\((\\bfunction|procedure\\b)?`;
+        const regConst = `${regComment}|((\\bfunction|procedure\\b)\\s+\\w+)|((\\w*.)\\s+(constant)\\s+(\\w+))|((\\b\\w+.\\b)\\s+(\\b\\w+\\b)(\\(|;))`;
         const regexp = new RegExp(regFind, 'gi');
         const regexpCons = new RegExp(regConst, 'gi');
 
@@ -24,19 +24,23 @@ export class PLSQLDocumentSymbolProvider implements vscode.DocumentSymbolProvide
         }
         let constant;
         while (constant = regexpCons.exec(text)) {
-            if (constant[2]) {
-                if (constant[1].toLowerCase() !== "function" && constant[1].toLowerCase() !== "procedure"){
-                    let line = document.lineAt(document.positionAt(constant.index));
-                    let icon;
-                    if(constant[1].toLowerCase().indexOf("constant") > 0)
-                       icon = vscode.SymbolKind.Constant;
-                    else icon = vscode.SymbolKind.Key;
-                    let symbolInfo = new vscode.SymbolInformation(
-                        constant[1]+" "+constant[2], icon,
-                        new vscode.Range(line.range.start, line.range.end));
-                    symbols.push(symbolInfo);
-                }
-                else return symbols;
+            if (constant[2]){
+                if(constant[2].toLowerCase() === "function" || constant[2].toLowerCase() === "procerdure" )
+                   break;
+            }
+            else if (constant[5]) {
+                let line = document.lineAt(document.positionAt(constant.index));
+                let symbolInfo = new vscode.SymbolInformation(
+                    constant[5].toLowerCase()+" "+constant[4].toLowerCase()+" "+constant[6].toLowerCase(), this.getSymbolKind(constant[5].toLowerCase()),
+                    new vscode.Range(line.range.start, line.range.end));
+                symbols.push(symbolInfo);
+            }
+            else if (constant[9]){
+                let line = document.lineAt(document.positionAt(constant.index));
+                let symbolInfo = new vscode.SymbolInformation(
+                    constant[8].toLowerCase()+" "+constant[9].toLowerCase(), vscode.SymbolKind.Key,
+                    new vscode.Range(line.range.start, line.range.end));
+                symbols.push(symbolInfo);
             }
         }
         return symbols;
@@ -48,6 +52,8 @@ export class PLSQLDocumentSymbolProvider implements vscode.DocumentSymbolProvide
             return vscode.SymbolKind.Function;
         else if (type === 'procedure')
             return vscode.SymbolKind.Method;
+        else if (type === 'constant')
+            return vscode.SymbolKind.Constant;
         else
             return vscode.SymbolKind.Package; // Specification or Body
     }
