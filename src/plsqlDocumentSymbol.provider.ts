54fd251a99d7322f9bead5721e0198b52627400e
zabel-xyz <zabel.xyz@gmail.com>
2018-07-05 20:18:01 +0200

    Symbol hierarchie

--- src/plsqlDocumentSymbol.provider.ts
+++ src/plsqlDocumentSymbol.provider.ts
@@ -1,10 +1,10 @@
 import * as vscode from 'vscode';
 import PlSqlParser from './plsqlParser.vscode';
-import PLSQLSettings from './plsql.settings';
+import { PLSQLSettings } from './plsql.settings';
 
 export class PLSQLDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 
-    public provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.SymbolInformation[] {
+    public provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.DocumentSymbol[] {
         PlSqlParser.initParser(PLSQLSettings.getCommentInSymbols());
         return PlSqlParser.getAllSymbols(document);
     }
--- src/plsqlParser.vscode.ts
+++ src/plsqlParser.vscode.ts
@@ -8,10 +8,9 @@ export default class PlSqlParserVSC extends PlSqlParser {
         return this.parseFile(document.fileName, document.getText());
     }
 
-    public static getAllSymbols(document: vscode.TextDocument): vscode.SymbolInformation[] {
-        return this.getSymbols(document.fileName, document.getText()).map(symbol =>
-            this.getSymbolInformation(document, symbol)
-        );
+    public static getAllSymbols(document: vscode.TextDocument): vscode.DocumentSymbol[] {
+        const root = this.parseFile(document.fileName, document.getText());
+        return root.symbols.map(symbol => this.getSymbolInformation(document, symbol));
     }
 
     public static getSymbolsCompletion(symbol: PLSQLSymbol): any {
@@ -24,12 +23,19 @@ export default class PlSqlParserVSC extends PlSqlParser {
     private static getSymbolInformation(document: vscode.TextDocument, symbol: PLSQLSymbol) {
         const line = symbol.offset != null ? document.lineAt(document.positionAt(symbol.offset)) : document.lineAt(symbol.line);
 
-        return new vscode.SymbolInformation(
+        const result = new vscode.DocumentSymbol(
             symbol.kindName+' '+symbol.name,
+            '',
             this.convertToSymbolKind(symbol.kind),
-            symbol.parent ? symbol.parent.kindName+' '+symbol.parent.name : '',
-            new vscode.Location(document.uri, new vscode.Range(line.range.start, line.range.end))
+            // symbol.parent ? symbol.parent.kindName+' '+symbol.parent.name : '',
+            // new vscode.Location(document.uri, new vscode.Range(line.range.start, line.range.end))
+            new vscode.Range(line.range.start, line.range.end),
+            new vscode.Range(line.range.start, line.range.end)
         );
+        if (symbol.symbols)
+            result.children = symbol.symbols.map(item => this.getSymbolInformation(document, item));
+
+        return result;
     }
 
     private static convertToSymbolKind(kind: PLSQLSymbolKind): vscode.SymbolKind {
